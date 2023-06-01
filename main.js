4cbd1f871ff324c3e3d9cf93577c80594c68aef2
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-06-01 19:50:14 -0300

    style: menu in mobile view

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('todo bien')
+const menuIcon = document.getElementById('menuIcon')
+const menuClose = document.getElementById('menuClose')
+
+menuIcon.addEventListener('click', toggleMenu)
+menuClose.addEventListener('click', toggleMenu)
+
+
+
+function toggleMenu (){
+    const menu = document.getElementById('menu')
+    menu.classList.toggle('inactive')
+}
