3ae7aafcc6d3354045f0a39b89f97b5127e8b675
Felipe Toro C <ftorocastilla@gmail.com>
2023-06-05 16:52:03 -0300

    feat: adjust the menu in desktop view

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const menuClose = document.getElementById('menuClose')
 menuIcon.addEventListener('click', toggleMenu)
 menuClose.addEventListener('click', toggleMenu)
 
-
-
 function toggleMenu (){
     const menu = document.getElementById('menu')
     menu.classList.toggle('inactive')
